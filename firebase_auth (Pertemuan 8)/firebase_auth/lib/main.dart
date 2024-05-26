7ebfca15858f795ce86596d4fc253dfdd13c3e3d
EcsaSi <21102147@ittelkom-pwt.ac.id>
2024-05-26 23:36:46 +0700

    Praktikum10

--- firebase_auth (Pertemuan 8)/firebase_auth/lib/main.dart
+++ firebase_auth (Pertemuan 8)/firebase_auth/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_09/bloc/login/login_cubit.dart';
 import 'package:firebase_auth_09/bloc/register/register_cubit.dart';
 import 'package:firebase_auth_09/firebase_options.dart';
+import 'package:firebase_auth_09/ui/home_screen.dart';
+import 'package:firebase_auth_09/ui/login.dart';
 import 'package:firebase_auth_09/ui/splash.dart';
 import 'package:firebase_auth_09/utils/routes.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -31,7 +34,22 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false, 
         navigatorKey: NAV_KEY, 
         onGenerateRoute: generateRoute, 
-        home: SplashScreen(),
+        home:  StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasData) {
+              return HomeScreen();
+            } else if (snapshot.hasError) {
+              return const Center(
+                child: Text('Something went wrong'),
+              );
+            } else {
+              return const LoginScreen();
+            }
+          },
+        ),
       ),
     );
   }
--- firebase_auth (Pertemuan 8)/firebase_auth/lib/ui/login.dart
+++ firebase_auth (Pertemuan 8)/firebase_auth/lib/ui/login.dart
@@ -1,6 +1,11 @@
-import 'package:flutter/material.dart'; 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth_09/ui/home_screen.dart';
+import 'package:firebase_auth_09/ui/phone_auth_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart'; 
 import 'package:flutter_bloc/flutter_bloc.dart'; 
 import 'package:firebase_auth_09/bloc/login/login_cubit.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import '../utils/routes.dart'; 
 
@@ -15,6 +20,18 @@ class _LoginScreenState extends State<LoginScreen> {
   final emailEdc = TextEditingController(); 
   final passEdc = TextEditingController(); 
   bool passInvisible = false; 
+
+  Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication gAuth = await gUser!.authentication;
+    final Credential = GoogleAuthProvider.credential(
+      accessToken: gAuth.accessToken,
+    );
+    return await FirebaseAuth.instance.signInWithCredential(Credential).then((value) async => await Navigator.pushAndRemoveUntil(
+      context, MaterialPageRoute(builder: (context) => HomeScreen()),
+      (route) => false));
+  }
+
   @override 
   Widget build(BuildContext context) { 
     return Scaffold( 
@@ -93,27 +110,65 @@ class _LoginScreenState extends State<LoginScreen> {
                 shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(10) 
                 ) 
                ), 
-               child: Text("Login", style: TextStyle( 
-                fontWeight: FontWeight.bold, fontSize: 24, color: Colors.white 
+               child: Text(
+                "Login", 
+                style: TextStyle( 
+                  fontWeight: FontWeight.bold, 
+                  fontSize: 24, 
+                  color: Colors.white 
                ),)), 
-               SizedBox(height: 25,), 
+               const SizedBox( 
+                height: 30.0, 
+               ), 
                Row( 
                   mainAxisAlignment: MainAxisAlignment.center, // Menengahkan elemen horizontal 
-                  children: [ 
-                    Text("Belum punya akun ?"), 
-                    TextButton(onPressed: (){ 
-                      Navigator.pushNamed(context, '/register'); 
-                    }, 
-                    child: Text("Daftar", style: TextStyle( 
-                      fontWeight: FontWeight.bold, 
-                      color: Color(0xff3D4DE0) 
-                    ),)) 
+                  children: [
+                    GestureDetector( 
+                      onTap: () {
+                        signInWithGoogle();
+                      }, 
+                      child: const CircleAvatar( 
+                        radius: 20.0, 
+                        backgroundImage: NetworkImage( 
+                          'https://img2.pngdownload.id/20190228/qby/kisspng-google-logo-google-account-g-suite-google-images-g-icon-archives-search-png-5c77ad39b77471.9286340315513470017515.jpg'), 
+                        ), 
+                      ),
+                      const SizedBox( 
+                        width: 30.0, 
+                      ), 
+                      GestureDetector( 
+                        onTap: () {
+                          Navigator.push( 
+                            context, 
+                            MaterialPageRoute( 
+                              builder: (context) => PhoneAuthScreen()));
+                        }, 
+                        child: const CircleAvatar( 
+                          radius: 20.0, 
+                          backgroundImage: NetworkImage( 
+                            'https://freepngimg.com/thumb/business/83615-blue-icons-symbol-telephone-computer-logo.png'), 
+                        ), 
+                      )
                   ], 
-                ) 
+                ),
+                SizedBox(height: 25,), 
+                  Row( 
+                    mainAxisAlignment: MainAxisAlignment.center, 
+                    children: [ 
+                      Text("Belum punya akun ?"), 
+                      TextButton(onPressed: (){ 
+                        Navigator.pushNamed(context, '/register'); 
+                      }, 
+                      child: Text("Daftar", style: TextStyle( 
+                        fontWeight: FontWeight.bold, 
+                      color: Color(0xff3D4DE0) 
+                      ),)) 
+                    ], 
+                  )  
               ], 
             ), 
           ), 
         ), 
       ); 
-    } 
+    }
   }
